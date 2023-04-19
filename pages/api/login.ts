d294f9dd53e5a8df0caf7941c549138556148b62
AthosF <athoseng@yahoo.com.br>
2023-04-19 08:22:46 -0300

    Correção senha

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -8,7 +8,7 @@ export default (
         const {login, senha} = req.body;
 
         if(login === 'admin@admin.com' &&
-           senha === "Admin@123"){
+           senha === 'Admin@123'){
                 res.status(200).json({msg : "Usuario autenticado com sucesso"});
            }
            return res.status(400).json({erro : 'Usuario ou senha nao encontrado'});
