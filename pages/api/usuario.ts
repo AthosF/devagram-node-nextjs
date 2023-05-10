3936284ea870d77c349dbe0518d63f7553557bd1
AthosF <athoseng@yahoo.com.br>
2023-05-09 21:26:20 -0300

    Carregamento do feed de postagens

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -1,9 +1,25 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
+import {conectarMongoDB} from '../../middlewares/conectarMongoDB'
+import { UsuarioModel } from '@/models/UsuarioModel';
 
 
-const usuarioEndPoint = (req : NextApiRequest, res : NextApiResponse) => {
-    return res.status(200).json('Usuario autenticado com sucesso');
+const usuarioEndPoint = async(req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any>) => {
+    // como eu pego os dados do usuario logado?
+    // id do usuario
+    try{
+        
+        const {userId} = req?.query;
+        //como eu busco todos os dados do meu usuario?
+        const usuario = await UsuarioModel.findById(userId);
+        return res.status(200).json(usuario);
+    }catch(e){
+        console.log(e);
+        return res.status(400).json({erro : 'Nao foi possivel obter dados do usuario'})
+    }
+
+    
 }
 
-export default validarTokenJWT(usuarioEndPoint);
+export default validarTokenJWT(conectarMongoDB(usuarioEndPoint));
